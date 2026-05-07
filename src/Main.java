0eef4ee8b0b80f63574e8cd95895857fd902e1e5
ricardo <ricardo.petri@unifil.br>
2026-05-06 21:38:19 -0300

    Implementação árvore binária

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
-        arvoreBinaria.inserir(15);
         arvoreBinaria.inserir(10);
+        arvoreBinaria.inserir(5);
         arvoreBinaria.inserir(1);
-        arvoreBinaria.inserir(4);
-        arvoreBinaria.inserir(2);
-        arvoreBinaria.inserir(9);
         arvoreBinaria.inserir(7);
-        arvoreBinaria.inserir(25);
-        arvoreBinaria.inserir(27);
-        arvoreBinaria.inserir(4325);
-        arvoreBinaria.percurso();
+        arvoreBinaria.inserir(5);
+        arvoreBinaria.percurso("Pos");
     }
 }
